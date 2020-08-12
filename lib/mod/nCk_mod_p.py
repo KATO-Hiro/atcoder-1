2fc5ff3f41a28ee860211dcdabf7f1abbb38253a
mts1104 <tsuyo.mts@gmail.com>
2020-08-13 02:55:31 +0900

    update

--- lib/mod/nCk_mod_p.py
+++ lib/mod/nCk_mod_p.py
@@ -1,28 +1,38 @@
 P = 10**9 + 7
 
 
-def make_fac_tables_mod_p(n, p):
-    fac = [0] * (n + 1)
-    fac_inv = [0] * (n + 1)
-    mod_inv = [0] * (n + 1)
-    fac[0] = fac[1] = 1
-    fac_inv[0] = fac_inv[1] = 1
-    mod_inv[1] = 1
-    for i in range(2, n + 1):
-        fac[i] = (fac[i - 1] * i) % p
-        mod_inv[i] = p - mod_inv[p % i] * (p // i) % p
-        fac_inv[i] = fac_inv[i - 1] * mod_inv[i] % p
-    return fac, fac_inv
+class MODCombination:
+    def __init__(self, n, p):
+        self.n = n
+        self.p = p  # MOD
+        self.fac, self.ifac = self.make_fac_tables_mod_p(n, p)
 
+    @staticmethod
+    def make_fac_tables_mod_p(n, p):
+        assert n >= 1
+        fac = [0] * (n + 1)   # _______ factorial: n!
+        ifac = [0] * (n + 1)  # inverse factorial: n!^{-1}
+        inv = [0] * (n + 1)   # inverse element  : n ^{-1}
+        fac[0] = fac[1] = 1
+        ifac[0] = ifac[1] = 1
+        inv[1] = 1
+        for i in range(2, n + 1):
+            fac[i] = fac[i - 1] * i % p
+            inv[i] = p - inv[p % i] * (p // i) % p
+            ifac[i] = ifac[i - 1] * inv[i] % p
+        return fac, ifac
 
-def nCk_mod_p(fac, fac_inv, n, k, p):
-    return ((fac[n] * fac_inv[n - k]) % p) * fac_inv[k] % p
+    def nCk(self, n, k):
+        # nCk = n! * k!^{-1} * (n-k)!^{-1}
+        return ((self.fac[n] * self.ifac[n - k]) % self.p) * self.ifac[k] % self.p
 
 
 def main():
+    print("n, k:")
     n, k = map(int, input().split())
-    fac, fac_inv = make_fac_tables_mod_p(n, P)
-    ans = nCk_mod_p(fac, fac_inv, n, k, P)
+
+    mod_comb = MODCombination(n, P)
+    ans = mod_comb.nCk(n, k)
     print(ans)
 
 
