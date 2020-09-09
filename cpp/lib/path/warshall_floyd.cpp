b61915e90ee474d6d4115eb3b9185218ddb738cd
mts1104 <tsuyo.mts@gmail.com>
2020-09-10 02:54:25 +0900

    update

--- cpp/lib/path/warshall_floyd.cpp
+++ cpp/lib/path/warshall_floyd.cpp
@@ -4,13 +4,17 @@ using ll = long long;
 // --------------------------------------------------------
 #define FOR(i,l,r) for (int i = (l); i < (r); ++i)
 #define REP(i,n) FOR(i,0,n)
-const int INF = 1 << 30;
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+const ll INF = 1LL << 62;
+// --------------------------------------------------------
 
 
-void warshall_floyd(int N, vector<vector<int>> &G) {
+void warshall_floyd(int N, vector<vector<ll>> &G) {
     REP(k, N) REP(i, N) {
         if (G[i][k] == INF) continue;
         REP(j, N) {
+            if (G[k][j] == INF) continue;
             if (G[i][j] > G[i][k] + G[k][j]) {
                 G[i][j] = G[i][k] + G[k][j];
             }
@@ -19,48 +23,42 @@ void warshall_floyd(int N, vector<vector<int>> &G) {
 }
 
 
+// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C
 int main() {
-    int N, M;
-    cin >> N >> M;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(10);
 
-    vector<vector<int>> G(N, vector<int>(N, INF));
-    REP(i, N) G[i][i] = 0;
+    int V, E; cin >> V >> E;
 
-    int a, b, t;
-    REP(_, M) {
-        cin >> a >> b >> t;
-        a--; b--;
-        G[a][b] = t;
-        G[b][a] = t;
+    VVLL G(V, VLL(V, INF));
+    REP(i, V) G[i][i] = 0LL;
+
+    REP(_, E) {
+        ll s, t, d; cin >> s >> t >> d;
+        G[s][t] = d;
     }
+    
+    warshall_floyd(V, G);
 
-    warshall_floyd(N, G);
-
-    int ans = INF;
-    REP(i, N) {
-        int max_dist = 0;
-        REP(j, N) max_dist = max(max_dist, G[i][j]);
-        ans = min(ans, max_dist);
+    bool is_negative_cycle = false;
+    REP(i, V) {
+        if (G[i][i] < 0) is_negative_cycle = true;
     }
-    cout << ans << endl;
+    if (is_negative_cycle) {
+        cout << "NEGATIVE CYCLE" << '\n';
+    } else {
+        REP(i, V) {
+            REP(j, V) {
+                if (j) cout << " ";
+                if (G[i][j] == INF) {
+                    cout << "INF";
+                } else {
+                    cout << G[i][j];
+                }
+            }
+            cout << '\n';
+        }
+    }
     return 0;
 }
-
-/*
-https://atcoder.jp/contests/abc012/tasks/abc012_4
-
-input 3
--------
-4 6
-1 2 1
-2 3 1
-3 4 1
-4 1 1
-1 3 1
-4 2 1
-
-output 3
---------
-1
-
-*/
