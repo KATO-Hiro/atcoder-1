54a352c828e460e98f919aceebdfed47bebea8f8
mts1104 <tsuyo.mts@gmail.com>
2020-09-23 01:00:18 +0900

    add

---
+++ cpp/lib/mod/modint.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+static const ll MOD = 1000000007;
+// static const ll MOD = 998244353;
+// --------------------------------------------------------
+
+
+struct mint {
+    ll x;
+    constexpr mint(ll x = 0) noexcept : x((x % MOD + MOD) % MOD) {}
+
+    constexpr mint& operator+=(const mint& a) noexcept {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a) noexcept {
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a) noexcept { (x *= a.x) %= MOD; return *this; }
+    constexpr mint& operator/=(const mint& a) noexcept { return *this *= a.inv(); }
+
+    constexpr mint operator-() const noexcept { return mint(-x); }
+    constexpr mint operator+(const mint& a) const noexcept { return mint(*this) += a; }
+    constexpr mint operator-(const mint& a) const noexcept { return mint(*this) -= a; }
+    constexpr mint operator*(const mint& a) const noexcept { return mint(*this) *= a; }
+    constexpr mint operator/(const mint& a) const noexcept { return mint(*this) /= a; }
+    constexpr bool operator==(const mint& a) const noexcept { return x == a.x; }
+    constexpr bool operator!=(const mint& a) const noexcept { return x != a.x; }
+
+    constexpr mint pow(ll n) const {
+        if (n == 0) return 1;
+        mint res = pow(n >> 1);
+        res *= res;
+        if (n&  1) res *= *this;
+        return res;
+    }
+    constexpr mint inv() const { return pow(MOD - 2); }
+
+    friend istream& operator>>(istream& is, mint& a) noexcept {
+        ll v; is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend ostream& operator<<(ostream& os, const mint& a) noexcept {
+        return os << a.x;
+    }
+};
+
+
+// Verify: https://atcoder.jp/contests/abc178/tasks/abc178_c
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(10);
+
+    int N; cin >> N;
+    mint ans = mint(10).pow(N) - mint(2) * mint(9).pow(N) + mint(8).pow(N);
+    cout << ans << '\n';
+
+    return 0;
+}
