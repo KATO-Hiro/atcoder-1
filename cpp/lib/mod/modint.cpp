bf37ff43429e6af9bc2070748a742c0186e41c18
mts1104 <tsuyo.mts@gmail.com>
2020-10-01 04:19:09 +0900

    update
    combination using modint

--- cpp/lib/mod/modint.cpp
+++ cpp/lib/mod/modint.cpp
@@ -8,11 +8,15 @@ static const ll MOD = 1000000007;
 
 
 // References:
-//   <https://github.com/atcoder/live_library/blob/master/mint.cpp>
-//   <https://noshi91.hatenablog.com/entry/2019/03/31/174006>
-//   <https://ei1333.github.io/luzhiled/snippets/math/mod-int.html>
-//   <https://gist.github.com/MiSawa/dc78c3eb3ca16051818759ea069e8ccb>
-//   <https://github.com/drken1215/algorithm/blob/master/MathCombinatorics/mod.cpp>
+//   mint:
+//     <https://github.com/atcoder/live_library/blob/master/mint.cpp>
+//     <https://noshi91.hatenablog.com/entry/2019/03/31/174006>
+//     <https://ei1333.github.io/luzhiled/snippets/math/mod-int.html>
+//     <https://gist.github.com/MiSawa/dc78c3eb3ca16051818759ea069e8ccb>
+//     <https://github.com/drken1215/algorithm/blob/master/MathCombinatorics/mod.cpp>
+//   combination:
+//     <https://github.com/atcoder/live_library/blob/master/comb.cpp>
+//     <https://github.com/drken1215/algorithm/blob/master/MathCombinatorics/mod.cpp>
 
 struct mint {
     ll x;
@@ -57,6 +61,60 @@ struct mint {
 };
 
 
+struct combination {
+    vector<mint> fact_, ifact_, inv_;
+    combination(int n) : fact_(n+1,0), ifact_(n+1,0), inv_(n+1,0) {
+        assert(n != 0);
+        assert(n < MOD);
+        fact_[0] = 1; fact_[1] = 1;
+        ifact_[0] = 1; ifact_[1] = 1;
+        inv_[1] = 1;
+        for(int i = 2; i <= n; ++i) {
+            fact_[i] = fact_[i-1] * i;
+            inv_[i] = -inv_[MOD%i] * (MOD/i);
+            ifact_[i] = ifact_[i-1] * inv_[i];
+        }
+    }
+
+    constexpr mint operator()(int n, int k) const noexcept {
+        if (n < 0 || k < 0 || n < k) return 0;
+        return fact_[n] * ifact_[n-k] * ifact_[k];
+    }
+    constexpr mint fact(int n) const noexcept {
+        if (n < 0) return 0;
+        return fact_[n];
+    }
+    constexpr mint ifact(int n) const noexcept {
+        if (n < 0) return 0;
+        return ifact_[n];
+    }
+    constexpr mint inv(int n) const noexcept {
+        if (n < 0) return 0;
+        return inv_[n];
+    }
+};
+
+
+// Verify: https://atcoder.jp/contests/abc034/tasks/abc034_c
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(10);
+
+    int W, H; cin >> W >> H;
+    W--; H--;
+
+    combination comb(W + H);
+    // mint ans = comb(W + H, W);
+    mint ans = comb.fact(W + H) * comb.ifact(W) * comb.ifact(H);
+
+    cout << ans << '\n';
+
+    return 0;
+}
+
+
+/*
 // Verify: https://atcoder.jp/contests/abc178/tasks/abc178_c
 int main() {
     ios::sync_with_stdio(false);
@@ -69,3 +127,4 @@ int main() {
 
     return 0;
 }
+*/
