6be86040fc4b7c671e6a8fba9bca0b93ebbe15a6
mts1104 <tsuyo.mts@gmail.com>
2020-10-05 23:16:20 +0900

    add

---
+++ cpp/lib/dp/digit_dp.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+template<class T> bool chmax(T& a, const T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> bool chmin(T& a, const T b) { if (b < a) { a = b; return 1; } return 0; }
+#define FOR(i,l,r) for (int i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(c) (c).begin(), (c).end()
+#define RALL(c) (c).rbegin(), (c).rend()
+#define SORT(c) sort(ALL(c))
+#define RSORT(c) sort(RALL(c))
+#define MIN(c) *min_element(ALL(c))
+#define MAX(c) *max_element(ALL(c))
+#define SUM(c) accumulate(ALL(c), 0)
+#define SUMLL(c) accumulate(ALL(c), 0LL)
+#define SZ(c) ((int)(c).size())
+#define debug(x) cerr << #x << " = " << (x) << '\n';
+using P = pair<int,int>;
+using VP = vector<P>;
+using VVP = vector<VP>;
+using VS = vector<string>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+using VB = vector<bool>;
+using VVB = vector<VB>;
+using VD = vector<double>;
+using VVD = vector<VD>;
+static const double EPS = 1e-10;
+static const double PI  = acos(-1.0);
+static const ll MOD = 1000000007;
+// static const ll MOD = 998244353;
+static const int INF = 1 << 30;  // 1073741824
+// static const ll INF = 1LL << 62;  // 4611686018427387904
+// --------------------------------------------------------
+// #include <atcoder/all>
+// using namespace atcoder;
+
+
+struct mint {
+    ll x;
+    constexpr mint(ll x = 0) noexcept : x((x % MOD + MOD) % MOD) {}
+
+    constexpr mint& operator+=(const mint& a) noexcept {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a) noexcept {
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a) noexcept { (x *= a.x) %= MOD; return *this; }
+    constexpr mint& operator/=(const mint& a) noexcept { return *this *= a.inv(); }
+
+    constexpr mint operator-() const noexcept { return mint(-x); }
+    constexpr mint operator+(const mint& a) const noexcept { return mint(*this) += a; }
+    constexpr mint operator-(const mint& a) const noexcept { return mint(*this) -= a; }
+    constexpr mint operator*(const mint& a) const noexcept { return mint(*this) *= a; }
+    constexpr mint operator/(const mint& a) const noexcept { return mint(*this) /= a; }
+    constexpr bool operator==(const mint& a) const noexcept { return x == a.x; }
+    constexpr bool operator!=(const mint& a) const noexcept { return x != a.x; }
+
+    constexpr mint pow(ll n) const {
+        if (n == 0) return 1;
+        mint res = pow(n >> 1);
+        res *= res;
+        if (n & 1) res *= *this;
+        return res;
+    }
+    constexpr mint inv() const { return pow(MOD - 2); }
+
+    friend istream& operator>>(istream& is, mint& a) noexcept {
+        ll v; is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend ostream& operator<<(ostream& os, const mint& a) noexcept {
+        return os << a.x;
+    }
+};
+using VM = vector<mint>;
+using VVM = vector<VM>;
+using VVVM = vector<VVM>;
+using VVVVM = vector<VVVM>;
+
+static const int MAX_L = 100001;
+static const int MAX_D = 100;
+VVVM dp(MAX_L + 1, VVM(2, VM(MAX_D, 0)));
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(10);
+
+    int D; cin >> D;
+    string N; cin >> N;
+    int L = SZ(N);
+    N = " " + N;
+
+    dp[0][0][0] = 1;
+    FOR(i, 1, L + 1) {
+        int n = N[i] - '0';
+        REP(j, 2) {
+            int max_d = (j == 1) ? 9 : n;
+            REP(d, max_d + 1) {
+                REP(r, D) {
+                    dp[i][j | (d < n)][(r + d) % D] += dp[i - 1][j][r];
+                }
+            }
+        }
+    }
+
+    // 正整数なので1を引く
+    mint ans = dp[L][0][0] + dp[L][1][0] - 1;
+    cout << ans << '\n';
+
+    return 0;
+}
