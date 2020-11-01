18d5dd36330dcf3f9eb5000158e37c130943b55c
mts1104 <tsuyo.mts@gmail.com>
2020-11-01 20:42:59 +0900

    update

--- cpp/lib/number/is_prime_numbers.cpp
+++ cpp/lib/number/is_prime_numbers.cpp
@@ -9,7 +9,8 @@ vector<bool> is_prime_numbers(ll n) {
     is_prime[0] = false;
     is_prime[1] = false;
     ll root_n = (ll)(sqrt(n) + 0.5);
-    for (int i = 2; i <= root_n; i++) {
+    for (int i = 4; i <= n; i += 2) is_prime[i] = false;
+    for (int i = 3; i <= root_n; i += 2) {
         for (int j = 2 * i; j <= n; j += i) {
             is_prime[j] = false;
         }
