154eebad082f71275cae1250fc131a8ff421af8e
mts1104 <tsuyo.mts@gmail.com>
2020-11-18 23:15:41 +0900

    update

--- cpp/lib/dp/tsp.cpp
+++ cpp/lib/dp/tsp.cpp
@@ -6,42 +6,42 @@ using ll = long long;
 #define REP(i,n) FOR(i,0,n)
 using VI = vector<int>;
 using VVI = vector<VI>;
-static const int INF = 1 << 30;
+static const int INF = (1 << 30) - 1;  // 1073741824 - 1
 // --------------------------------------------------------
 
 
-static const int MAX_V = 15;
-VVI dp((1 << MAX_V), VI(MAX_V, INF));
+static const int MAX_N = 16;
+VVI dp((1 << MAX_N), VI(MAX_N, INF));
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout << fixed << setprecision(10);
 
-    int V, E; cin >> V >> E;
-    VVI G(V, VI(V, INF));
-    REP(_, E) {
+    int N, M; cin >> N >> M;
+    VVI G(N, VI(N, INF));
+    REP(_, M) {
         int s, t, d; cin >> s >> t >> d;
         G[s][t] = d;
     }
 
     // dp[T][v] --> dp[S][u] に遷移 (T + {u} = S)
-    // - 頂点 u は状態 S に含まれる必要がある
-    // - 頂点 v は状態 T に含まれる必要があるが，
-    //   そうでないときは dp[T][v] = INF なので気にせず計算可能
+    // - u ∈ S
+    // - v ∈ T
     dp[0][0] = 0;  // 「各頂点を1度ずつ通る」であれば出発点によらず最終結果は同じ(∵閉路)
-    FOR(S, 1, (1 << V)) {
-        REP(u, V) {
+    FOR(S, 1, (1 << N)) {
+        REP(u, N) {
             if (!(S & (1 << u))) continue;
             int T = S - (1 << u);
-            REP(v, V) {
+            REP(v, N) {
+                if (T != 0 && !(T & (1 << v))) continue;  // この問題では消してよい (∵次行で弾かれる)
                 if (G[v][u] == INF) continue;
                 dp[S][u] = min(dp[S][u], dp[T][v] + G[v][u]);
             }
         }
     }
 
-    int ans = dp[(1 << V) - 1][0];
+    int ans = dp[(1 << N) - 1][0];
     if (ans == INF) ans = -1;
     cout << ans << '\n';
 
