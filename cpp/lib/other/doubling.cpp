3427da04febf30569b6d3d958a41ca21241ff7f8
mts1104 <tsuyo.mts@gmail.com>
2020-12-04 23:05:59 +0900

    small change

--- cpp/lib/other/doubling.cpp
+++ cpp/lib/other/doubling.cpp
@@ -13,22 +13,22 @@ int main() {
     cin.tie(0);
     cout << fixed << setprecision(15);
 
-    ll N, K; cin >> N >> K;
+    ll N, T; cin >> N >> T;
     VI A(N+1); FOR(i,1,N+1) cin >> A[i];
 
-    int D = 1;  // 二進数表記で K を表現するために必要な桁数
-    while ((1LL << D) <= K) D++;
+    int K = 1;  // 二進数表記で T を表現するために必要な桁数
+    while ((1LL << K) <= T) K++;
 
-    VVI db(D, VI(N+1));
+    VVI db(K, VI(N+1));
     FOR(i,1,N+1) db[0][i] = A[i];
 
-    FOR(k,1,D) FOR(i,1,N+1) {
+    FOR(k,1,K) FOR(i,1,N+1) {
         db[k][i] = db[k-1][db[k-1][i]];
     }
 
     int pos = 1;
-    FOR(k,0,D) {
-        if (K & (1LL << k)) {
+    FOR(k,0,K) {
+        if (T & (1LL << k)) {
             pos = db[k][pos];
         }
     }
