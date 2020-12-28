dcc25b9e9b490b0bdf10f5dc21f0970f7705a148
mts1104 <tsuyo.mts@gmail.com>
2020-12-28 20:46:19 +0900

    add

---
+++ cpp/lib/other/big_integer.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+// --------------------------------------------------------
+
+// reference: https://boostjp.github.io/tips/multiprec-int.html
+#include <boost/multiprecision/cpp_int.hpp>
+typedef boost::multiprecision::cpp_int bint;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    bint A; cin >> A;
+    bint B; cin >> B;
+
+    bint ans = A + B;
+    cout << ans << '\n';
+
+    return 0;
+}
+// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_2_A&lang=ja
+//     please use language: C++ (not C++17)
