22fbb3500e7109894276ee43751a85ce7214b008
mts1104 <tsuyo.mts@gmail.com>
2021-01-03 14:26:21 +0900

    small change

--- cpp/lib/graph/scc.cpp
+++ cpp/lib/graph/scc.cpp
@@ -5,6 +5,7 @@ using ll = long long;
 #define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
 #define REP(i,n) FOR(i,0,n)
 #define SZ(c) ((ll)(c).size())
+using VLL = vector<ll>;
 // --------------------------------------------------------
 #include <atcoder/scc>
 using namespace atcoder;
@@ -16,7 +17,39 @@ int main() {
     cout << fixed << setprecision(15);
 
     ll N, M; cin >> N >> M;
+    scc_graph G(N);
+    ll s, t;
+    REP(_,M) {
+        cin >> s >> t;
+        G.add_edge(s, t);
+    }
 
+    auto scc = G.scc();
+    VLL scc_id(N);  // 各頂点の強連結成分の番号
+    REP(i,SZ(scc)) {
+        for (auto& u : scc[i]) {
+            scc_id[u] = i;
+        }
+    }
+
+    ll Q; cin >> Q;
+    ll u, v;
+    while (Q--) {
+        cin >> u >> v;
+        cout << (scc_id[u] == scc_id[v] ? 1 : 0) << '\n';
+    }
+
+    return 0;
+}
+// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C&lang=ja
+
+/*
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    ll N, M; cin >> N >> M;
     scc_graph G(N);
     ll a, b;
     REP(_,M) {
@@ -37,3 +70,4 @@ int main() {
     return 0;
 }
 // Verify: https://atcoder.jp/contests/practice2/tasks/practice2_g
+*/
