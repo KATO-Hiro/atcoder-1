d3f38105c7ddd82ebf3eb6b8a90dcbd6fb5ca488
mts1104 <tsuyo.mts@gmail.com>
2021-01-04 13:49:46 +0900

    add

---
+++ cpp/lib/dp/edpc/O_Matching.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+// --------------------------------------------------------
+#include <atcoder/modint>
+using namespace atcoder;
+
+using mint = modint1000000007;
+using VM = vector<mint>;
+using VVM = vector<VM>;
+
+// NOTE
+//   - bit DP
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    ll N; cin >> N;
+    VVLL A(N, VLL(N)); REP(i,N) REP(j,N) cin >> A[i][j];
+
+    // dp[S] := S[i]=1の女性にペアが存在する場合の数
+    VM dp(1<<N, 0);
+    dp[0] = 1;
+    FOR(S,1,1<<N) {
+        ll i = __builtin_popcount(S);  // 何人目の男性か
+        REP(j,N) {
+            if (!(S & (1<<j))) continue;
+            if (A[i-1][j] == 0) continue;
+            dp[S] += dp[S - (1<<j)];
+        }
+    }
+    ll ans = dp[(1<<N) - 1].val();
+    cout << ans << endl;
+
+    return 0;
+}
+// https://atcoder.jp/contests/dp/tasks/dp_o
