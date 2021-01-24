2af6d18a35edfcbe0fcfcea14bee293431d1b396
mts1104 <tsuyo.mts@gmail.com>
2021-01-24 17:07:01 +0900

    small change

--- cpp/lib/dp/lcs.cpp
+++ cpp/lib/dp/lcs.cpp
@@ -10,14 +10,14 @@ using VVLL = vector<VLL>;
 // --------------------------------------------------------
 
 
-int lcs(string X, string Y) {
+ll lcs(string X, string Y) {
     ll N = SZ(X);
     ll M = SZ(Y);
     X = " " + X;
     Y = " " + Y;
 
-    VVLL dp(N + 1, VLL(M + 1, 0));
-    FOR(i, 1, N + 1) FOR(j, 1, M + 1) {
+    VVLL dp(N+1, VLL(M+1, 0));
+    FOR(i,1,N+1) FOR(j,1,M+1) {
         if (X[i] == Y[j]) {
             dp[i][j] = dp[i-1][j-1] + 1;
         } else {
@@ -28,14 +28,13 @@ int lcs(string X, string Y) {
 }
 
 
-// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout << fixed << setprecision(10);
 
-    int Q; cin >> Q;
-    REP(_, Q) {
+    ll Q; cin >> Q;
+    while (Q--) {
         string X; cin >> X;
         string Y; cin >> Y;
         cout << lcs(X, Y) << '\n';
@@ -43,3 +42,4 @@ int main() {
 
     return 0;
 }
+// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C&lang=ja
