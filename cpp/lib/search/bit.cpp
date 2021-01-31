5714445d3c22fe96b3c649f20fc2d0062b965c01
mts1104 <tsuyo.mts@gmail.com>
2021-02-01 00:01:38 +0900

    small change

--- cpp/lib/search/bit.cpp
+++ cpp/lib/search/bit.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
 using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define BIT(b,i) ((b>>i) & 1)
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+// --------------------------------------------------------
 
 
-
-// https://atcoder.jp/contests/abc128/tasks/abc128_c
 int main() {
-    int N, M;
-    cin >> N >> M;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
 
-    vector<vector<int>> s(M);
-    int k, t;
-    rep(i, M) {
-        cin >> k;
-        rep(j, k) {
-            cin >> t;
-            s[i].push_back(t - 1);
+    ll N, M; cin >> N >> M;
+    VVLL s(M);
+    REP(i,M) {
+        ll k; cin >> k;
+        REP(j,k) {
+            ll t; cin >> t;
+            s[i].push_back(t-1);
         }
     }
-    vector<int> p(M);
-    rep(i, M) cin >> p[i];
+    VLL p(M); REP(i,M) cin >> p[i];
 
-    int ans = 0;
-    for (int bit = 0; bit < (1 << N); bit++) {
+    ll ans = 0;
+    REP(S,1<<N) {
         bool are_light = true;
-        for (int i = 0; i < M; i++) {
-            int count = 0;
-            for (auto ss : s[i]) {
-                if (bit & (1 << ss)) count++;
-            }
+        REP(i,M) {
+            ll count = 0;
+            for (auto& ss : s[i]) if (BIT(S,ss)) count++;
             if (count % 2 != p[i]) are_light = false;
         }
         if (are_light) ans++;
     }
     cout << ans << endl;
+
     return 0;
 }
+// Verify: https://atcoder.jp/contests/abc128/tasks/abc128_c
