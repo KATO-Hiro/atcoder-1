d62f7963faf231fcb869f6f676670fee3afcad77
mts1104 <tsuyo.mts@gmail.com>
2021-01-31 23:52:00 +0900

    small change

--- cpp/lib/dp/tsp.cpp
+++ cpp/lib/dp/tsp.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 using ll = long long;
 // --------------------------------------------------------
+template<class T> bool chmin(T& a, const T b) { if (b < a) { a = b; return 1; } return 0; }
 #define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
 #define REP(i,n) FOR(i,0,n)
+#define BIT(n,i) ((n>>i) & 1)
 using VLL = vector<ll>;
 using VVLL = vector<VLL>;
 static const ll INF = (1LL << 62) - 1;  // 4611686018427387904 - 1
@@ -23,25 +25,22 @@ int main() {
         G[s][t] = d;
     }
 
-    // dp[T][v] --> dp[S][u] に遷移 (T + {u} = S)
-    // - u ∈ S
-    // - v ∈ T
-    VVLL dp((1 << N), VLL(N, INF));
+    // dp[S][u] --> dp[T][v]  配るDP
+    //   - u ∈ S
+    //   - v ∈ T = S + {v}
     // 出発点が指定されていない場合，「各頂点を1度ずつ通る」であれば出発点によらず
-    // 最終結果は同じであるため (∵閉路)，ここでは簡単のため頂点1のみ出発点とする
+    // 最終結果は同じであるため (∵閉路)，頂点1を出発点とした場合のみ解けば十分
+    VVLL dp(1<<N, VLL(N, INF));
     dp[0][0] = 0;
-    FOR(S, 1, (1 << N)) {
-        REP(u, N) {
-            if (!(S & (1 << u))) continue;
-            ll T = S - (1 << u);
-            REP(v, N) {
-                if (T != 0 && !(T & (1 << v))) continue;  // この問題では消してよい (∵次行で弾かれる)
-                if (G[v][u] == INF) continue;
-                dp[S][u] = min(dp[S][u], dp[T][v] + G[v][u]);
+    REP(S,1<<N) {
+        REP(u,N) {
+            REP(v,N) if (!BIT(S,v)) {
+                ll T = S | (1<<v);
+                chmin(dp[T][v], dp[S][u] + G[u][v]);
             }
         }
     }
-    ll ans = dp[(1 << N) - 1][0];
+    ll ans = dp[(1<<N) - 1][0];
     if (ans == INF) ans = -1;
     cout << ans << '\n';
 
