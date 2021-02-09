28ced2eafb8749a5f7a90d88cd3b4523dc7f061b
mts1104 <tsuyo.mts@gmail.com>
2021-02-09 20:24:37 +0900

    small change

--- cpp/lib/search/permutation.cpp
+++ cpp/lib/search/permutation.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 using ll = long long;
 // --------------------------------------------------------
-#define FOR(i,l,r) for (int i = (l); i < (r); ++i)
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
 #define REP(i,n) FOR(i,0,n)
 #define ALL(c) (c).begin(), (c).end()
-using VI = vector<int>;
+using VLL = vector<ll>;
 // --------------------------------------------------------
 
 
-// Verify: https://atcoder.jp/contests/abc150/tasks/abc150_c
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout << fixed << setprecision(10);
 
-    int N; cin >> N;
-    VI P(N); REP(i,N) cin >> P[i];
-    VI Q(N); REP(i,N) cin >> Q[i];
+    ll N; cin >> N;
+    VLL P(N); REP(i,N) cin >> P[i];
+    VLL Q(N); REP(i,N) cin >> Q[i];
 
-    VI R(N); iota(ALL(R), 1);
-    int i = 1;
-    int a = 1, b = 1;
+    VLL R(N); iota(ALL(R), 1);
+    ll i = 1;
+    ll a = 1, b = 1;
     do {
         if (equal(ALL(P), ALL(R))) a = i;
         if (equal(ALL(Q), ALL(R))) b = i;
         i++;
     } while (next_permutation(ALL(R)));
 
-    int ans = abs(a - b);
+    ll ans = llabs(a - b);
     cout << ans << '\n';
 
     return 0;
 }
+// Verify: https://atcoder.jp/contests/abc150/tasks/abc150_c
