03a8dc893f98d024be4823bc560e15166fc821c0
mts1104 <tsuyo.mts@gmail.com>
2021-02-23 11:59:14 +0900

    update

--- cpp/lib/number/is_prime_number.cpp
+++ cpp/lib/number/is_prime_number.cpp
@@ -1,49 +1,59 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
 using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+// --------------------------------------------------------
+
+
+// bool is_prime_number(ll n) {
+//     if (n == 1) return false;
+//     if (n == 2) return true;
+//     if (n % 2 == 0) return false;
+
+//     ll f = 3, root_n = (ll)(sqrt(n) + 0.5);
+//     while (f <= root_n) {
+//         if (n % f == 0) return false;
+//         f += 2;
+//     }
+//     return true;
+// }
 
 
 bool is_prime_number(ll n) {
+    assert(1 <= n);
     if (n == 1) return false;
     if (n == 2) return true;
+    if (n == 3) return true;
     if (n % 2 == 0) return false;
+    if (n % 3 == 0) return false;
 
-    ll f = 3, root_n = (ll)(sqrt(n) + 0.5);
-    while (f <= root_n) {
-        if (n % f == 0) return false;
-        f += 2;
+    // 3 より大きい素数は 6k±1 で表される
+    ll root = floor(sqrt(n) + 0.5);
+    ll f = 6;
+    while (f-1 <= root) {
+        if (n % (f-1) == 0) return false;
+        if (n % (f+1) == 0) return false;
+        f += 6;
     }
     return true;
 }
 
 
 int main() {
-    int n, a, ans = 0;
-    cin >> n;
-    rep(i, n) {
-        cin >> a;
-        if (is_prime_number(a)) ans++;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    ll ans = 0;
+    ll Q; cin >> Q;
+    while (Q--) {
+        ll N; cin >> N;
+        if (is_prime_number(N)) ans++;
     }
     cout << ans << endl;
+
     return 0;
 }
-
-
-/*
-https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_C
-
-input 1
--------
-6
-2
-3
-4
-5
-6
-7
-
-output 1
---------
-4
-*/
+// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C&lang=ja
