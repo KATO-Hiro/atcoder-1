b9d0950151a0413e79e4503258e18f400242fd84
mts1104 <tsuyo.mts@gmail.com>
2021-03-06 13:45:44 +0900

    add

---
+++ cpp/lib/geometry/arg_sort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+template<class T> bool chmax(T& a, const T b) { if (a < b) { a = b; return 1; } return 0; }
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(c) (c).begin(), (c).end()
+using P = pair<ll,ll>;
+using VP = vector<P>;
+// --------------------------------------------------------
+
+using ld = long double;
+
+// TODO: 厳密には long double でも誤差の問題はある
+//       外積で誤差なく処理する方法があるらしい (AtCoder で必要になるかは微妙)
+
+// 偏角ソート: 座標を偏角で昇順ソート．偏角の取りうる値の範囲は (-π,π] (∵ atan2)
+// Reference: <https://qiita.com/ganariya/items/adef1a7f89ae88b804da>
+void arg_sort(VP& points) {
+    auto comp = [](const P& p1, const P& p2) -> bool {
+        auto& [x1, y1] = p1;
+        auto& [x2, y2] = p2;
+        return atan2l(y1, x1) < atan2l(y2, x2);
+    };
+    sort(ALL(points), comp);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    ll N; cin >> N;
+    VP points(N); REP(i,N) cin >> points[i].first >> points[i].second;
+
+    arg_sort(points);
+
+    ld ans = 0;
+    REP(l,N) {
+        ld x = 0, y = 0;
+        FOR(r,l,l+N) {
+            x += points[r % N].first;
+            y += points[r % N].second;
+            chmax(ans, sqrt(x*x + y*y));
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+// Verify: https://atcoder.jp/contests/abc139/tasks/abc139_f
