88ae2b9fdea1ca20e42410b2360161321c28c0fd
mts1104 <tsuyo.mts@gmail.com>
2021-03-30 22:41:14 +0900

    small change

--- cpp/lib/dp/edpc/U_Grouping.cpp
+++ cpp/lib/dp/edpc/U_Grouping.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 using ll = long long;
 // --------------------------------------------------------
+template<class T> bool chmax(T& a, const T b) { if (a < b) { a = b; return 1; } return 0; }
 #define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
 #define REP(i,n) FOR(i,0,n)
+#define BIT(b,i) (((b)>>(i)) & 1)
 using VLL = vector<ll>;
 using VVLL = vector<VLL>;
 static const ll INF = (1LL << 62) - 1;  // 4611686018427387904 - 1
@@ -22,17 +24,19 @@ int main() {
     ll N; cin >> N;
     VVLL A(N,VLL(N)); REP(i,N) REP(j,N) cin >> A[i][j];
 
+    // dp[S] := 状態 S に対応するうさぎたちをグループ分けした時の総得点の最大値
     VLL dp(1<<N,0);
     REP(S,1<<N) {
-        // S を 1 グループにした場合の総得点で初期化
-        REP(i,N) if (S & (1<<i)) {
-            FOR(j,i+1,N) if (S & (1<<j)) {
+        // S を 1 グループにした場合の総得点で dp[S] を初期化
+        REP(i,N) if (BIT(S,i)) {
+            FOR(j,i+1,N) if (BIT(S,j)) {
                 dp[S] += A[i][j];
             }
         }
         // S における 2 グループ分割の方法を全探索 (≒ 区間 DP)
+        //    <https://kyopro-friends.hatenablog.com/entry/2019/01/12/231106>
         for (ll T = (S-1) & S; 0 < T; T = (T-1) & S) {
-            dp[S] = max(dp[S], dp[T] + dp[T^S]);  // T^S: S における T の補集合 S\T
+            chmax(dp[S], dp[T] + dp[T^S]);  // T^S: S における T の補集合 S\T
         }
     }
     ll ans = dp[(1<<N) - 1];
