9c1ebf55085a982c026d311b78d77b9920fdd17b
mts1104 <tsuyo.mts@gmail.com>
2021-04-08 21:14:45 +0900

    add

---
+++ cpp/lib/dp/tdpc/O_string.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+#define RALL(c) (c).rbegin(), (c).rend()
+#define RSORT(c) sort(RALL(c))
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+static const ll MOD = 1000000007;
+// --------------------------------------------------------
+
+// NOTE
+//   - 挿入 DP
+
+
+struct mint {
+    ll x;
+    constexpr mint(ll x = 0) noexcept : x((x % MOD + MOD) % MOD) {}
+
+    constexpr mint& operator+=(const mint& a) noexcept {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a) noexcept {
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a) noexcept { (x *= a.x) %= MOD; return *this; }
+    constexpr mint& operator/=(const mint& a) noexcept { return *this *= a.inv(); }
+
+    constexpr mint operator-() const noexcept { return mint(-x); }
+    constexpr mint operator+(const mint& a) const noexcept { return mint(*this) += a; }
+    constexpr mint operator-(const mint& a) const noexcept { return mint(*this) -= a; }
+    constexpr mint operator*(const mint& a) const noexcept { return mint(*this) *= a; }
+    constexpr mint operator/(const mint& a) const noexcept { return mint(*this) /= a; }
+    constexpr bool operator==(const mint& a) const noexcept { return x == a.x; }
+    constexpr bool operator!=(const mint& a) const noexcept { return x != a.x; }
+
+    constexpr mint pow(ll n) const {
+        if (n == 0) return 1;
+        mint res = pow(n >> 1);
+        res *= res;
+        if (n & 1) res *= *this;
+        return res;
+    }
+    constexpr mint inv() const { return pow(MOD - 2); }
+
+    friend istream& operator>>(istream& is, mint& a) noexcept {
+        ll v; is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend ostream& operator<<(ostream& os, const mint& a) noexcept {
+        return os << a.x;
+    }
+};
+using VM = vector<mint>;
+using VVM = vector<VM>;
+using VVVM = vector<VVM>;
+using VVVVM = vector<VVVM>;
+
+
+struct combination {
+    vector<mint> fact_, ifact_, inv_;
+    int n_;
+    combination() {}
+    combination(int n) : fact_(n+1,0), ifact_(n+1,0), inv_(n+1,0) {
+        assert(n != 0);
+        assert(n < MOD);
+        n_ = n;
+        fact_[0] = 1; fact_[1] = 1;
+        ifact_[0] = 1; ifact_[1] = 1;
+        inv_[1] = 1;
+        for(int i = 2; i <= n; ++i) {
+            fact_[i] = fact_[i-1] * i;
+            inv_[i] = -inv_[MOD%i] * (MOD/i);
+            ifact_[i] = ifact_[i-1] * inv_[i];
+        }
+    }
+
+    mint P(const int& n, const int& k) const noexcept {
+        if (n < 0 || k < 0 || n < k) return 0;
+        return fact_[n] * ifact_[n-k];
+    }
+    mint C(const int& n, const int& k) const noexcept {
+        if (n < 0 || k < 0 || n < k) return 0;
+        return fact_[n] * ifact_[n-k] * ifact_[k];
+    }
+    mint H(const int& n, const int& k) const noexcept {
+        if (n < 0 || k < 0) return 0;
+        assert(n + k - 1 <= n_);
+        return C(n + k - 1, k);
+    }
+    mint fact(const int& n) const noexcept {
+        assert(n <= n_);
+        if (n < 0) return 0;
+        return fact_[n];
+    }
+    mint ifact(const int& n) const noexcept {
+        assert(n <= n_);
+        if (n < 0) return 0;
+        return ifact_[n];
+    }
+    mint inv(const int& n) const noexcept {
+        assert(n <= n_);
+        if (n < 0) return 0;
+        return inv_[n];
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(15);
+
+    VLL F(26); REP(i,26) cin >> F[i];
+
+    RSORT(F);
+    ll N = 0, sum_f = 0;
+    for (ll f : F) {
+        if (f == 0) break;
+        N++; sum_f += f;
+    }
+
+    combination Z(sum_f+2);
+
+    // dp[i][j] := i 番目の英小文字まで見た時，同じ文字が j 箇所で隣り合う場合の数
+    VVM dp(N+1,VM(sum_f+1,0)); dp[0][0] = 1;
+    ll cum_f = 0;
+    FOR(i,1,N+1) {
+        ll f = F[i-1];
+        // k := i 番目の英小文字を f[i] 個挿入する際の分割数
+        // l := j 箇所のうち次の文字 (分割済) を挿入する箇所の数
+        REP(j,cum_f+1) FOR(k,1,f+1) REP(l,min(j,k)+1) {  // 
+            dp[i][j-l+(f-k)] += dp[i-1][j] * Z.C(j,l) * Z.C(cum_f+1-j,k-l) * Z.C(f-1,k-1);
+        }
+        cum_f += f;
+    }
+    mint ans = dp[N][0];
+    cout << ans << endl;
+
+    return 0;
+}
+// https://atcoder.jp/contests/tdpc/tasks/tdpc_string
