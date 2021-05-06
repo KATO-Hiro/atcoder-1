02e21a7a959ec08dd246810a034e0885c9251e7d
mts1104 <tsuyo.mts@gmail.com>
2021-05-06 19:37:02 +0900

    small change

--- cpp/lib/flow/maxflow.cpp
+++ cpp/lib/flow/maxflow.cpp
@@ -15,17 +15,20 @@ int main() {
     cout << fixed << setprecision(15);
 
     ll N, M; cin >> N >> M;
+
+    // ll s = N, t = s + 1;
+    // mf_graph<ll> G(N+2);
     ll s = 0, t = N - 1;
-    mf_graph<ll> G(N + 1);
-    ll u, v, c;
+    mf_graph<ll> G(N);
     REP(_,M) {
-        cin >> u >> v >> c;
+        ll u, v, c; cin >> u >> v >> c;
+        // u--; v--;
         G.add_edge(u, v, c);
     }
 
     ll ans = G.flow(s, t);
-    cout << ans << endl;;
+    cout << ans << endl;
 
     return 0;
 }
-// Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A&lang=ja
+// Verify: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A&lang=ja
