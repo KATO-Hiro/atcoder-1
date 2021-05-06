ce8d8563cb278c55b57f4d96219f8cad6a0f284d
mts1104 <tsuyo.mts@gmail.com>
2021-05-06 19:46:25 +0900

    small change

--- cpp/lib/flow/bipartite_matching.cpp
+++ cpp/lib/flow/bipartite_matching.cpp
@@ -15,13 +15,13 @@ int main() {
     cout << fixed << setprecision(15);
 
     ll X, Y, E; cin >> X >> Y >> E;
-    mf_graph<ll> G(X + Y + 2);
     ll s = X + Y, t = s + 1;
+    mf_graph<ll> G(X + Y + 2);
     REP(x, X) G.add_edge(s, x, 1);
-    REP(y, Y) G.add_edge(y + X, t, 1);
+    REP(y, Y) G.add_edge(X + y, t, 1);
     REP(_, E) {
         ll x, y; cin >> x >> y;
-        G.add_edge(x, y + X, 1);
+        G.add_edge(x, X + y, 1);
     }
     cout << G.flow(s, t) << '\n';
 
