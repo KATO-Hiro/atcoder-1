ae3a882d3d62411dee6027ba5fb178258c3f3219
mts1104 <tsuyo.mts@gmail.com>
2021-06-03 22:40:44 +0900

    add

---
+++ cpp/lib/algebra/gauss_jordan_modP.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+// static const ll MOD = 1000000007;
+static const ll MOD = 1000003;  // for ecr063_e
+// --------------------------------------------------------
+
+struct mint {
+    ll x;
+    constexpr mint(ll x = 0) noexcept : x((x % MOD + MOD) % MOD) {}
+
+    constexpr mint& operator+=(const mint& a) noexcept {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator-=(const mint& a) noexcept {
+        if ((x += MOD - a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    constexpr mint& operator*=(const mint& a) noexcept { (x *= a.x) %= MOD; return *this; }
+    constexpr mint& operator/=(const mint& a) noexcept { return *this *= a.inv(); }
+
+    constexpr mint operator-() const noexcept { return mint(-x); }
+    constexpr mint operator+(const mint& a) const noexcept { return mint(*this) += a; }
+    constexpr mint operator-(const mint& a) const noexcept { return mint(*this) -= a; }
+    constexpr mint operator*(const mint& a) const noexcept { return mint(*this) *= a; }
+    constexpr mint operator/(const mint& a) const noexcept { return mint(*this) /= a; }
+    constexpr bool operator==(const mint& a) const noexcept { return x == a.x; }
+    constexpr bool operator!=(const mint& a) const noexcept { return x != a.x; }
+
+    constexpr mint pow(ll n) const {
+        if (n == 0) return 1;
+        mint res = pow(n >> 1);
+        res *= res;
+        if (n & 1) res *= *this;
+        return res;
+    }
+    constexpr mint inv() const { return pow(MOD - 2); }
+
+    friend istream& operator>>(istream& is, mint& a) noexcept {
+        ll v; is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend ostream& operator<<(ostream& os, const mint& a) noexcept {
+        return os << a.x;
+    }
+};
+using VM = vector<mint>;
+using VVM = vector<VM>;
+using VVVM = vector<VVM>;
+using VVVVM = vector<VVVM>;
+
+
+// References:
+//   <https://drken1215.hatenablog.com/entry/2019/03/20/202800>
+
+
+// Gauss Jordan の掃き出し法 (行列 A を標準形に変形する)
+int GaussJordan(vector<vector<mint>>& A, bool is_extended = false) {
+    int H = A.size(), W = A[0].size();
+    int rank = 0;
+    for (int w = 0; w < W; w++) {
+        if (is_extended && w == W - 1) break;  // 拡大係数行列の最終列はスキップ
+
+        int pivot = -1;
+        for (int h = rank; h < H; h++) {
+            if (A[h][w] != 0) { pivot = h; break; }
+        }
+        if (pivot == -1) continue;
+
+        swap(A[pivot], A[rank]);
+
+        // ピボットの値を 1 にする (逆元をかける)
+        mint inv = A[rank][w].inv();
+        for (int ww = 0; ww < W; ++ww)
+            A[rank][ww] = A[rank][ww] * inv;
+
+        // ピボットのある列を掃き出す
+        for (int h = 0; h < H; ++h) if (h != rank) {
+            if (A[h][w] != 0) {
+                mint c = A[h][w];  // 掃き出したい列の係数
+                for (int ww = 0; ww < W; ww++) {
+                    A[h][ww] -= A[rank][ww] * c;
+                }
+            }
+        }
+        rank++;
+    }
+    return rank;
+}
+
+// 連立一次方程式 Ax = b を解く
+pair<int,vector<mint>> linear_equation(const vector<vector<mint>>& A, const vector<mint>& b) {
+    int H = A.size(), W = A[0].size();
+    vector<vector<mint>> M(H, vector<mint>(W + 1));
+    for (int h = 0; h < H; h++) {
+        for (int w = 0; w < W; w++) {
+            M[h][w] = A[h][w];
+        }
+        M[h][W] = b[h];
+    }
+    int rank = GaussJordan(M, true);
+
+    // 解が存在するか判定
+    for (int h = rank; h < H; h++) {
+        if (M[h][W] != 0) return make_pair(-1, vector<mint>(0));
+    }
+
+    vector<mint> res(W, 0);
+    for (int h = 0; h < rank; h++) {
+        res[h] = M[h][W];
+    }
+    return make_pair(rank, res);
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+
+    ll K = 10;
+    VVM F(K+1, VM(K+1));
+    VM B(K+1);
+
+    REP(h,K+1) {
+        REP(w,K+1) F[h][w] = mint(h).pow(w);
+
+        cout << "? " << h << endl;
+        ll fx; cin >> fx;
+        assert(fx != -1);
+        B[h] = fx;
+    }
+
+    auto [rank, A] = linear_equation(F, B);
+
+    REP(x,MOD) {
+        mint fx = 0;
+        REP(k,K+1) fx += A[k] * mint(x).pow(k);
+        if (fx == 0) {
+            cout << "! " << x << endl;
+            return 0;
+        }
+    }
+    cout << "! -1" << endl;
+
+    return 0;
+}
+// Verify: https://codeforces.com/contest/1155/problem/E
---
+++ cpp/lib/graph/centroid_decomposition.cpp
@@ -0,0 +1,167 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+// --------------------------------------------------------
+#define FOR(i,l,r) for (ll i = (l); i < (r); ++i)
+#define REP(i,n) FOR(i,0,n)
+using P = pair<ll,ll>;
+using VP = vector<P>;
+using VVP = vector<VP>;
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+// --------------------------------------------------------
+
+
+// References:
+//   <https://ei1333.github.io/luzhiled/snippets/tree/centroid-decomposition.html>
+//   <https://tjkendev.github.io/procon-library/cpp/graph/centroid-decomposition.html>
+//   <https://atcoder.jp/contests/yahoo-procon2018-final-open/submissions/2376520>
+
+/**
+ * @brief 重心分解 (Centroid Decomposition)
+ * 
+ */
+struct CentroidDecomposition {
+    int N;
+    vector<vector<int>> G;
+    vector<int> sz;  // 頂点 u を根とする部分木のサイズ
+    vector<bool> centroid;  // 頂点 u が重心として使用されているか
+    CentroidDecomposition(int n) : N(n) {
+        G.resize(N);
+        sz.resize(N);
+        centroid.resize(N);
+    }
+
+    // 双方向に辺を張る
+    void add_edge(int u, int v) {
+        assert(0 <= u && u < N);
+        assert(0 <= v && v < N);
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    // 現時点の部分木のサイズを求める
+    int dfs_sz(int u, int p) {
+        int s = 1;
+        for (int v : G[u]) if (v != p) {
+            if (centroid[v]) continue;
+            s += dfs_sz(v, u);
+        }
+        return sz[u] = s;
+    }
+
+    // 重心を探索する
+    int search_centroid(int u, int p, int cc_sz) {
+        for (int v : G[u]) if (v != p) {
+            if (centroid[v]) continue;
+            if (sz[v] > cc_sz / 2) return search_centroid(v, u, cc_sz);
+        }
+        return u;
+    }
+
+    // 重心分解
+    //   - c_root  : 重心の根
+    //   - c_graph : 重心から次の重心に有向辺を張ったグラフ
+    int build(vector<vector<int>>& c_graph) {
+        c_graph.resize(N);
+        auto dfs = [&](auto self, int u) -> int {
+            int c = search_centroid(u, -1, dfs_sz(u, -1));
+            centroid[c] = true;
+            for (int v : G[c]) {
+                if (centroid[v]) continue;
+                c_graph[c].push_back(self(self, v));
+            }
+            centroid[c] = false;
+            return c;
+        };
+        return dfs(dfs, 0);
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+
+    ll N, Q; cin >> N >> Q;
+    VVLL G(N);
+    CentroidDecomposition cd(N);
+    REP(_,N-1) {
+        ll u, v; cin >> u >> v;
+        u--; v--;
+        G[u].push_back(v);
+        G[v].push_back(u);
+        cd.add_edge(u, v);
+    }
+
+    vector<vector<int>> c_graph;
+    int c_root = cd.build(c_graph);
+
+    VVP E(N);
+    REP(i,Q) {
+        ll u, k; cin >> u >> k;
+        u--;
+        E[u].push_back(P(k, i));
+    }
+
+    VLL ans(Q, 0);
+
+    vector<bool> c_used(N, false);  // 重心として使用中か
+    auto dfs = [&](auto self, ll c) -> void {
+        c_used[c] = true;
+
+        // (1) 頂点 u, v が共に 1 つの部分木に含まれるような組 (u, v)
+        for (auto u : c_graph[c]) {
+            self(self, u);
+        }
+
+        // (2) 頂点 u, v が異なる部分木に含まれるような組 (u, v)
+        // (3) 重心 c と他の頂点 u との組 (c, u)
+        map<ll,ll> mp_all;  // ある距離の個数
+        VP dist_all;        // (頂点, 重心までの距離)
+        mp_all[0]++; dist_all.push_back(P(c, 0));  // 重心 c の分 : (3)
+        for (auto u : G[c]) {
+            if (c_used[u]) continue;
+
+            // 部分木に含まれる全頂点について，重心までの距離を計算
+            map<ll,ll> mp;
+            VP dist;
+            auto dfs = [&](auto self, ll u, ll p, ll d) -> void {
+                mp[d]++; dist.push_back(P(u, d));
+                for (auto v : G[u]) if (v != p) {
+                    if (c_used[v]) continue;
+                    self(self, v, u, d + 1);
+                }
+            };
+            dfs(dfs, u, c, 1);
+
+            // 重複分を引いておく : (2)(3) で (1) を数えないようにするため
+            for (auto [v, d] : dist) {
+                for (auto [k, i] : E[v]) {
+                    ans[i] -= mp[k - d];
+                }
+            }
+
+            // マージ
+            dist_all.insert(dist_all.end(), dist.begin(), dist.end());
+            for (auto [d, n] : mp) mp_all[d] += n;
+        }
+        // (2)(3) の計算の本体
+        for (auto [v, d] : dist_all) {
+            for (auto [k, i] : E[v]) {
+                ans[i] += mp_all[k - d];
+            }
+        }
+
+        c_used[c] = false;
+    };
+    dfs(dfs, c_root);
+
+    for (ll a : ans) {
+        cout << a << '\n';
+    }
+
+    return 0;
+}
+// Verify: https://atcoder.jp/contests/yahoo-procon2018-final-open/tasks/yahoo_procon2018_final_c
