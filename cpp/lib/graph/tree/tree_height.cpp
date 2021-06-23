db8b0bb045910c0fba4538ad7d3d2d94ecfdc43d
mts1104 <tsuyo.mts@gmail.com>
2021-06-24 08:07:00 +0900

    small change

--- cpp/lib/graph/tree/tree_height.cpp
+++ cpp/lib/graph/tree/tree_height.cpp
@@ -15,9 +15,12 @@ static const ll INF = (1LL << 62) - 1;  // 4611686018427387904 - 1
 // --------------------------------------------------------
 
 
+/** NOTE: 本実装は O(N^2) (スターグラフの場合)
+ *        全方位木 DP によって O(N) で計算可能 **/
+
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
     cout << fixed << setprecision(15);
 
     ll N; cin >> N;
@@ -29,22 +32,22 @@ int main() {
         G[t].push_back(P(s, w));
     }
 
-    // dp[r][k] := 根とした頂点 r から出る k 番目の辺に関する部分木の高さ
+    // dp[u][i] := 頂点 u を根とした場合に頂点 u と隣接する i 番目の部分木の高さ
     VVLL dp(N);
-    REP(u,N) dp[u] = VLL(SZ(G[u]), -INF);
-    auto rec = [&](auto self, ll i, ll j) -> ll {
-        if (dp[i][j] >= 0) return dp[i][j];
-        auto& [u, w] = G[i][j];
+    REP(u,N) dp[u] = VLL(SZ(G[u]), -1);
+    auto dfs = [&](auto self, ll u, ll i) -> ll {
+        if (dp[u][i] >= 0) return dp[u][i];
         ll res = 0;
-        REP(v, SZ(G[u])) if (G[u][v].first != i) {
-            chmax(res, self(self, u, v));
+        auto [v, w] = G[u][i];
+        REP(j,SZ(G[v])) if (G[v][j].first != u) {
+            chmax(res, self(self, v, j));
         }
-        return dp[i][j] = w + res;
+        return dp[u][i] = w + res;
     };
 
     REP(u,N) {
         ll ans = 0;
-        REP(v,SZ(G[u])) chmax(ans, rec(rec, u, v));
+        REP(i,SZ(G[u])) chmax(ans, dfs(dfs, u, i));
         cout << ans << '\n';
     }
 
