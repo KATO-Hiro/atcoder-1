49ddbbe2b1c0ae6a064475768b9f7ac188e5d51a
mts1104 <tsuyo.mts@gmail.com>
2021-06-27 15:32:18 +0900

    small change

--- cpp/lib/number/pow_mod.cpp
+++ cpp/lib/number/pow_mod.cpp
@@ -1,26 +1,35 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
 using ll = long long;
+// --------------------------------------------------------
+static const ll MOD = 1000000007;
+// --------------------------------------------------------
 
 
 ll pow_mod(ll x, ll n, ll mod) {
     ll res = 1;
     if (n > 0) {
         res = pow_mod(x, n / 2, mod);
-        if (n % 2 == 0) res = (res * res) % mod;
-        else res = (((res * res) % mod) * x) % mod;
+        if (n % 2 == 0) {
+            res = (res * res) % mod;
+        } else {
+            res = (((res * res) % mod) * x) % mod;
+        }
     }
     return res;
 }
 
 
 int main() {
-    const ll MOD = 1e+9 + 7;
-    ll m, n;
-    cin >> m >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+
+    ll m, n; cin >> m >> n;
+
     ll ans = pow_mod(m, n, MOD);
     cout << ans << endl;
+
     return 0;
 }
 // Verify: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B&lang=ja
